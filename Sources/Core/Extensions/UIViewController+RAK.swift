e5d1db0e2f8c6adb19919104094e5e102cf26f3f
Rakuyo <rakuyo.mo@gmail.com>
2024-05-10 11:15:12 +0800

    feat: Add multiple extension tools

---
+++ Sources/Core/Extensions/UIViewController+RAK.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewController+RAK.swift
+//  RakuyoKit
+//
+//  Created by Rakuyo on 2024/5/10.
+//  Copyright © 2024 RakuyoKit. All rights reserved.
+//
+
+#if !os(watchOS)
+import UIKit
+
+extension Extendable where Base: UIViewController {
+    @available(iOSApplicationExtension, unavailable)
+    public func hideKeyboard() {
+        UIApplication.shared.rak.keyWindow?.endEditing(true)
+    }
+
+    public func addChild(_ childController: UIViewController, toView: UIView) {
+        base.addChild(childController)
+        childController.beginAppearanceTransition(true, animated: false)
+
+        if let stackView = toView as? UIStackView {
+            stackView.addArrangedSubview(childController.view)
+        } else {
+            toView.addSubview(childController.view)
+        }
+
+        childController.endAppearanceTransition()
+        childController.didMove(toParent: base)
+    }
+}
+#endif
---
+++ Sources/Core/Extensions/WidgetConfiguration+RAK.swift
@@ -0,0 +1,22 @@
+//
+//  WidgetConfiguration+RAK.swift
+//  RakuyoKit
+//
+//  Created by Rakuyo on 2024/5/10.
+//  Copyright © 2024 RakuyoKit. All rights reserved.
+//
+
+#if !os(tvOS) && !os(visionOS)
+import SwiftUI
+
+import WidgetKit
+
+@available(iOS 14.0, macOS 11.0, watchOS 9.0, *)
+@available(tvOS, unavailable)
+extension WidgetConfiguration {
+    public func contentMarginsDisabledForGlobal() -> some WidgetConfiguration {
+        if #available(iOS 15.0, *) { return contentMarginsDisabled() }
+        return self
+    }
+}
+#endif
