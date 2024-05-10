8e67476852fc2554b22c37a230d88c764e4a9082
Rakuyo <rakuyo.mo@gmail.com>
2024-05-10 10:39:25 +0800

    feat: Add Add `DispatchQueue` extension

---
+++ Sources/Core/Extensions/DispatchQueue+RAK.swift
@@ -0,0 +1,53 @@
+//
+//  DispatchQueue+RAK.swift
+//  RakuyoKit
+//
+//  Created by Rakuyo on 2024/5/10.
+//  Copyright © 2024 RakuyoKit. All rights reserved.
+//
+
+import Foundation
+
+extension Extendable where Base: DispatchQueue {
+    public typealias Task = (_ canceled: Bool) -> Void
+
+    /// Create a deferred task that can be canceled midway using the `cancel()` method.
+    ///
+    /// - Parameters:
+    ///   - time: Delay duration
+    ///   - task: Tasks to be performed after the time is reached
+    /// - Returns: Action to cancel execution
+    @discardableResult
+    public static func after(
+        _ time: DispatchTimeInterval,
+        execute task: @escaping EmptyClosure
+    ) -> Task? {
+        func _after(block: @escaping EmptyClosure) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + time, execute: block)
+        }
+
+        var taskAction: EmptyClosure? = task
+        var result: Task?
+
+        let delayedClosure: Task = { canceled in
+            if let action = taskAction, !canceled {
+                DispatchQueue.main.async(execute: action)
+            }
+            taskAction = nil
+            result = nil
+        }
+
+        result = delayedClosure
+
+        _after { result?(false) }
+
+        return result
+    }
+    
+    /// Cancel delayed tasks
+    ///
+    /// - Parameter task: Task to be canceled
+    public static func cancel(_ task: Task?) {
+        task?(true)
+    }
+}
