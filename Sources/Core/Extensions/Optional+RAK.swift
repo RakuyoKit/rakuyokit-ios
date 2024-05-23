6f4728cff4a5cd48f13abf0899228aa98ee8324e
Rakuyo <rakuyo.mo@gmail.com>
2024-05-23 18:03:16 +0800

    feat: Add Optional+RAK

---
+++ Sources/Core/Extensions/Optional+RAK.swift
@@ -0,0 +1,16 @@
+//
+//  Optional+RAK.swift
+//  RakuyoKit
+//
+//  Created by Rakuyo on 2024/5/23.
+//  Copyright © 2024 RakuyoKit. All rights reserved.
+//
+
+import Foundation
+
+extension GenericExtendable where Base == Generic? {
+    /// If the value is `nil`, returns the `"nil"` string. Otherwise the string interpolation of the value itself is returned.
+    public var safeDescription: String {
+        base.flatMap { "\($0)" } ?? "nil"
+    }
+}
