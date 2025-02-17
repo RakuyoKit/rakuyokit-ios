9316a78fed609293fda9a32c2f26fe151826fd39
Rakuyo <rakuyo.mo@gmail.com>
2025-02-17 22:39:45 +0800

    feat: Enhanced RAKCore module

---
+++ Sources/Core/Utilities/ForceError.swift
@@ -0,0 +1,28 @@
+//
+//  ForceError.swift
+//  RakuyoKit
+//
+//  Created by Rakuyo on 2025/2/17.
+//  Copyright © 2024 RakuyoKit. All rights reserved.
+//
+
+import Foundation
+
+/// Forceful error type conversion
+public enum ForceError {
+    /// Forcefully converts an error type to the target type
+    ///
+    /// Suitable for APIs that do not yet provide typed errors
+    @discardableResult
+    public static func convert<Success, Error: Swift.Error>(
+        to _: Error.Type,
+        _ body: () async throws -> Success
+    ) async throws(Error) -> Success {
+        do {
+            return try await body()
+        } catch {
+            // swiftlint:disable:next force_cast
+            throw error as! Error
+        }
+    }
+}
